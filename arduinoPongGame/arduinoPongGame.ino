5c16cdc58589471ec3bbb6e20e83fbc80fb8d5c6
preston <polston13@outlook.com>
2024-09-20 16:25:19 -0400

    added logic for the moving pong pixel and the bouncing pixel as of now they do not work at the same time

--- arduinoPongGame/arduinoPongGame.ino
+++ arduinoPongGame/arduinoPongGame.ino
@@ -8,12 +8,19 @@ int lifeThree = 9;
 int upButton = 2;
 int downButton = 4;
 int br = 115200;
-int delayTime = 75;
+int delayTime = 250;
+int movementDelay = 75;
 int moveUp;
 int moveDown;
 int pongPosition;
 int pongX = 0;
 int pongY = 0;
+int xPos = 3;
+int yPos = 2;
+int deltaX = 1;
+int deltaY = 1;
+int i;
+int j;
 
 void setup() {
   pinMode(lifeOne, OUTPUT);
@@ -29,20 +36,46 @@ void loop() {
 digitalWrite(lifeOne, HIGH);
 digitalWrite(lifeTwo, HIGH);
 digitalWrite(lifeThree, HIGH);
-frame[pongX][pongY] = !frame[pongX][pongY];
+
+memset(frame, 0, sizeof(frame)); 
+
+frame[pongX][pongY] = 1;
+
 matrix.renderBitmap(frame, 8, 12);
-moveUp = digitalRead(upButton);
-  if (moveUp == 0){
-    pongX = pongX + 1;
+
+if (digitalRead(upButton) == HIGH){
+  if (pongX > 0) { 
+    pongX--;
     delay(delayTime);
     Serial.println("moving up");
   }
-moveDown = digitalRead(downButton);
-Serial.println(moveDown);
-  if (moveDown == 0){
-    pongX = pongX - 1;
+}
+  
+if (digitalRead(downButton) == HIGH){
+  if (pongX < 7) { 
+    pongX++;
     delay(delayTime);
     Serial.println("moving down");
   }
+}
 
+  for(i = 0; i < 8; i= i + 1) {
+    for(j = 0; j < 12; j = j + 1) {
+      frame[i][j] = 0;
+      if(i == xPos && j == yPos){
+        frame[i][j] = 1;
+      }
+    }
+  }
+
+  if(xPos >= 7 || xPos <= 0){
+    deltaX = -deltaX;
+  }
+  if(yPos >= 11 || yPos <= 0){
+    deltaY = -deltaY;
+  }
+matrix.renderBitmap(frame, 8, 12);
+  xPos += deltaX;
+  yPos += deltaY;
+  delay(movementDelay);
 }
